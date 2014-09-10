1cac8bf6811a424b5c9d6de201d98062e809feaa
ShyanTanDeB <debshyantan@gmail.com>
2014-09-10 10:50:30 +0530

    new tab icon for userscreen

--- IGEF/gen/com/google/android/gms/R.java
+++ IGEF/gen/com/google/android/gms/R.java
@@ -99,8 +99,8 @@ public final class R {
 		public static final int ic_plusone_small_off_client = 0x7f02008c;
 		public static final int ic_plusone_standard_off_client = 0x7f02008d;
 		public static final int ic_plusone_tall_off_client = 0x7f02008e;
-		public static final int powered_by_google_dark = 0x7f0200a0;
-		public static final int powered_by_google_light = 0x7f0200a1;
+		public static final int powered_by_google_dark = 0x7f0200a2;
+		public static final int powered_by_google_light = 0x7f0200a3;
 	}
 	public static final class id {
 		public static final int book_now = 0x7f050034;
